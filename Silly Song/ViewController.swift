84c219edc17fc92df23731e75a82f17ff4caa146
Luis Fernando <xvleg3ndaryxv@gmail.com>
2017-05-14 08:38:08 -0500

    Logic's app implemented, only problems with constraints

--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false;
+    }
+}
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var lyricsView: UITextView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        nameField.delegate = self
+     //   self.nameField.autocapitalizationType = .Words
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func reset(_ sender: Any) {
+        nameField.text = "";
+        lyricsView.text = "";
+        
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+        let name: String = nameField.text!;
+        let carl:String = template(name: name);
+        lyricsView.text = carl;
+    }
 
+   
 }
+
+func template(name: String)->String{
+    let lol: String = cutName(carl: name);
+    let carl: String = name+" "+name+" "+"Bo"+" B"+lol+" Banana Fana Fo F"+lol+" Me My Mo m"+lol+" "+name;
+    print(carl);
+    return  carl.lowercased();
+}
+
+
+func cutName(carl: String)-> String{
+    var shortname: String = carl;
+    
+    var carl: String = String(shortname.characters.reversed());
+    
+    let  finalResult = String(carl.characters.suffix(3));
+    
+    return finalResult;
+}
+
+
+
+
 
